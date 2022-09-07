05280114e0554590eadcbeb39e5f9cb8286ddf37
Alexander Spatar <younax@yandex.ru>
2022-09-07 14:11:22 +0300

    VC-1: Edit eslint rules and .editorconfig

--- src/components/forms/sign-in/SignIn.tsx
+++ src/components/forms/sign-in/SignIn.tsx
@@ -1,7 +1,26 @@
-// import classes from './SignIn.module.scss';
+import * as Yup from 'yup';
+import classes from './SignIn.module.scss';
 
-const SignIn = () => (
-  <div>Hello world!</div>
-);
+const SignIn = (): JSX.Element => {
+  const validationSchema = Yup.object().shape({
+    login: Yup.string()
+      .min(8, 'Логин должен содержать не менее 8 символов')
+      .max(30, 'Логин не должен превышать 30 символов')
+      .required('Поле "Логин" обязательно к заполнению'),
+  });
+
+  const initVal = {
+    login: '',
+    password: '',
+  };
+
+  return (
+    <section className={classes.wrapper}>
+      <div className={classes.container}>
+        <h2>Sign In</h2>
+      </div>
+    </section>
+  );
+};
 
 export default SignIn;
