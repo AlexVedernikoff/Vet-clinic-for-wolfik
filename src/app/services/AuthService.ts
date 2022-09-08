f67df51db7d9452d4f604c3bfa9a8d1d580c2304
nischmann <aziretsuiuntbek@gmail.com>
2022-09-09 00:01:43 +0600

    VC-4 refactoring AuthServices.ts

--- src/app/services/AuthService.ts
+++ src/app/services/AuthService.ts
@@ -1,70 +1,65 @@
 import axios from 'axios';
 import { LoginUser, NewUser } from '../../types/AuthDTO';
 
-class AuthService {
-  baseUrl = 'http://91.241.64.154:8080/';
+const baseUrl = 'http://91.241.64.154:8080/';
+const token = '';
 
-  token = '';
+export const createNewUser = async (data: NewUser) => {
+  try {
+    const {
+      email, firstname, lastname, password, confirmPassword,
+    } = data;
+    const response = await axios.post(
+      `${baseUrl}api/registration`,
+      {
+        email,
+        firstname,
+        lastname,
+        password,
+        confirmPassword,
+      },
+      { headers: { 'Content-type': 'application/json' } },
+    );
 
-  async createNewUser (data: NewUser) {
-    try {
-      const {
-        email, firstname, lastname, password, confirmPassword,
-      } = data;
-      const response = await axios.post(
-        `${this.baseUrl}api/registration`,
-        {
-          email,
-          firstname,
-          lastname,
-          password,
-          confirmPassword,
-        },
-        { headers: { 'Content-type': 'application/json' } },
-      );
-
-      return response.data;
-    } catch (e) {
-      console.log(e);
-    }
+    return response.data;
+  } catch (e) {
+    console.log(e);
   }
+};
 
-  async loginUser (data: LoginUser) {
-    try {
-      const {
-        username, password,
-      } = data;
-      const response = await axios.post(
-        `${this.baseUrl}api/auth`,
-        {
-          username,
-          password,
-        },
-        { headers: { 'Content-type': 'application/json' } },
-      );
+export const loginUser = async (data: LoginUser) => {
+  try {
+    const {
+      username, password,
+    } = data;
+    const response = await axios.post(
+      `${baseUrl}api/auth`,
+      {
+        username,
+        password,
+      },
+      { headers: { 'Content-type': 'application/json' } },
+    );
 
-      return response.data;
-    } catch (e) {
-      console.log(e);
-    }
+    return response.data;
+  } catch (e) {
+    console.log(e);
   }
+};
 
-  async getCurrentClient () {
-    try {
-      const res = await axios.get(
-        `${this.baseUrl}api/auth/getCurrent`,
-        {
-          headers: {
-            Authorization: this.token,
-          },
+export const getCurrentClient = async () => {
+  try {
+    const res = await axios.get(
+      `${baseUrl}api/auth/getCurrent`,
+      {
+        headers: {
+          Authorization: token,
         },
-      );
+      },
+    );
 
-      return res.data;
-    } catch (e) {
-      console.log(e);
-    }
+    return res.data;
+  } catch (e) {
+    console.log(e);
   }
-}
-
-export default AuthService;
+};
