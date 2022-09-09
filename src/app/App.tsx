db261d45f1e5502ae8a9a9a9f33352f7e42b61bd
Annet-Garkusha <annetkovna15@gmail.com>
2022-09-09 17:39:01 +0300

    #5 create a project header

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { SignUp } from '../components/forms/sing-up/SignUp';
+import { Header } from '../components/header/Header';
 
 function App (): JSX.Element {
   return (
     <Router>
-      <h2>Header</h2>
+      <Header />
       <Routes>
         <Route path="/" element={<h2>Main</h2>} />
         <Route path="/signUp" element={<SignUp />} />
