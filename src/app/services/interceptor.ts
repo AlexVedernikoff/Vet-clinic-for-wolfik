be57d00aac647bb85a59e3d1db432e4c7f1c560c
Annet-Garkusha <annetkovna15@gmail.com>
2022-09-15 14:39:38 +0300

    fixes task 8

--- src/app/services/interceptor.ts
+++ src/app/services/interceptor.ts
@@ -1,17 +1,19 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig, AxiosInstance } from 'axios';
 
-const authToken = `eyJhbGciOiJIUzUxMiJ9.
-eyJzdWIiOiJjbGllbnQxQGVtYWlsLmNvbSIsImlhdCI6MTY2Mjk5MDkxNywiZXhwIjoxNjY0OTkwOTE3fQ.
-i3ntbCy9kPlKIA6ICbkmNhNw-BbeB8i-WBIr6V_xZDau4VEIn6AYaRlHTTSaVTimcbiQkztKJJyr7-3Q_IGnIg`;
+// const authToken = `eyJhbGciOiJIUzUxMiJ9.
+// eyJzdWIiOiJjbGllbnQxQGVtYWlsLmNvbSIsImlhdCI6MTY2Mjk5MDkxNywiZXhwIjoxNjY0OTkwOTE3fQ.
+// i3ntbCy9kPlKIA6ICbkmNhNw-BbeB8i-WBIr6V_xZDau4VEIn6AYaRlHTTSaVTimcbiQkztKJJyr7-3Q_IGnIg`;
 
-export const axiosInstance = axios.create({
+export const axiosInstance: AxiosInstance = axios.create({
   baseURL: 'http://91.241.64.154:8080/',
-  headers: { Authorization: `${authToken}` },
 
 });
 
-axiosInstance.interceptors.request.use(async (req) => {
-  console.log('interceptors run');
+axiosInstance.interceptors.request.use((config: AxiosRequestConfig): AxiosRequestConfig => {
+  const { headers } = config;
+  if (headers) {
+    headers.Authorization = `Bearer ${localStorage.getItem('AUTH_TOKEN')}`;
+  }
 
-  return req;
+  return config;
 });
