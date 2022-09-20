d4a302aefe30a48103d1cd82e58d5ad8ebf321f5
Annet-Garkusha <annetkovna15@gmail.com>
2022-09-20 13:21:05 +0300

    add main page forum

---
+++ src/pages/forum-page.tsx
@@ -0,0 +1,10 @@
+import styles from '../styles/global.module.scss';
+
+const ForumPage = ():JSX.Element => (
+  <div className={styles.wrapper}>
+
+    <h3 className={styles.title}>Форум</h3>
+  </div>
+);
+
+export default ForumPage;
