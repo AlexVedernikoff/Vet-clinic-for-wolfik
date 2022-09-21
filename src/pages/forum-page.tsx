4b93f2a6d953e55cf864e08a4a96062f7d5b9869
Annet-Garkusha <annetkovna15@gmail.com>
2022-09-21 13:37:14 +0300

    add topic form

--- src/pages/forum-page.tsx
+++ src/pages/forum-page.tsx
@@ -1,9 +1,11 @@
+import Topic from '../components/topic/Topic';
 import styles from '../styles/global.module.scss';
 
 const ForumPage = ():JSX.Element => (
   <div className={styles.wrapper}>
 
     <h3 className={styles.title}>Форум</h3>
+    <Topic />
   </div>
 );
 
