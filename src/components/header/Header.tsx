b18fd8cc5b62051874775a7e24927fa50f190d1e
RobotYkrop <dendozzor@gmail.com>
2022-09-26 17:48:45 +0500

    fix all

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,12 @@
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
-import { Button, Layout } from 'antd';
+import { Button, Dropdown, Layout } from 'antd';
 import styles from './Header.module.scss';
+import Bell from '../../assets/icons/Bell.svg';
+import Avatar from '../../assets/icons/Avatar.png';
+import Arrow from '../../assets/icons/ArrowPanel.svg';
+import Menu from '../../assets/icons/MenuHeader.svg';
+import '../../styles/global.scss';
 
 export const Header = (): JSX.Element => {
   type Header = { label: string; path: string }[];
@@ -17,19 +22,61 @@ export const Header = (): JSX.Element => {
     { label: 'Форум', path: '/forumPage' },
   ];
 
+  const Bool = false;
+
   return (
     <Layout.Header className={styles.wrapper}>
-      {links.map((el) => (
-        <Link to={`${el.path}`} key={uuidv4()} className={styles.link}>
-          {el.label}
-        </Link>
-      ))}
-      <Link to="/sign-in">
-        <Button className={styles.signin}>sign-in</Button>
-      </Link>
-      <Link to="/sign-up">
-        <Button className={styles.registration}>registration</Button>
-      </Link>
+      <div className={styles.linksBlock}>
+        {window.innerWidth >= 1224 ? (
+          links.map((el) => (
+            <NavLink to={`${el.path}`} key={uuidv4()} className={styles.link}>
+              {el.label}
+            </NavLink>
+          ))
+        ) : (
+          <Dropdown
+            overlay={(
+              <div className={styles.menu}>
+                {links.map((el) => (
+                  <Link
+                    to={`${el.path}`}
+                    key={uuidv4()}
+                    className={styles.link}
+                  >
+                    {el.label}
+                  </Link>
+                ))}
+              </div>
+            )}
+            placement="bottom"
+            arrow={{ pointAtCenter: true }}
+          >
+            <img src={Menu} alt="Menu" />
+          </Dropdown>
+        )}
+      </div>
+      {Bool ? (
+        <div className={styles.autorization}>
+          <Link to="/sign-in">
+            <Button className={styles.signin}>sign-in</Button>
+          </Link>
+          <Link to="/sign-up">
+            <Button className={styles.registration}>registration</Button>
+          </Link>
+        </div>
+      ) : (
+        <div className={styles.panel}>
+          <img className={styles.bell} src={Bell} alt="Уведомления" />
+          <div className={styles.user}>
+            <div className={styles.username}>
+              <span>ThePetOwner</span>
+              <span className={styles.role}>Клиент</span>
+            </div>
+            <img className={styles.arrow} src={Arrow} alt="Avatar" />
+            <img src={Avatar} alt="Avatar" />
+          </div>
+        </div>
+      )}
     </Layout.Header>
   );
 };
