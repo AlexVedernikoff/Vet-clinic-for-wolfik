0f4f11d36615fa4fa73001f6ae78a4dab7f7d354
RobotYkrop <dendozzor@gmail.com>
2022-09-28 12:07:44 +0500

    added API

---
+++ src/store/auth/dewormingApi.ts
@@ -0,0 +1,46 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { Procedure, ProcedureResponse } from '../../types/PetsDTO/procedure';
+
+export const dewormingApi = createApi({
+  reducerPath: 'dewormingApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://91.241.64.154:8080/',
+  }),
+  endpoints: (build) => ({
+    getDewormingPet: build.query<ProcedureResponse, number>({
+      query: (Id: number) => `api/client/procedure/deworming?petId=${Id}`,
+    }),
+    addNewDeworming: build.mutation<any, number>({
+      query: (Id: number) => ({
+        url: `api/client/procedure/deworming?petId=${Id}`,
+        method: 'post',
+        body: Id,
+      }),
+    }),
+    updateDeworming: build.mutation<any, number>({
+      query: (Id: number) => ({
+        url: `api/client/procedure/deworming/${Id}`,
+        method: 'put',
+        body: Id,
+      }),
+    }),
+    deleteDeworming: build.mutation<any, number>({
+      query: (Id: number) => ({
+        url: `api/client/procedure/deworming/${Id}`,
+        method: 'delete',
+        body: Id,
+      }),
+    }),
+    getDeworming: build.query<ProcedureResponse, number>({
+      query: (Id: number) => `api/client/procedure/deworming/${Id}`,
+    }),
+  }),
+});
+
+export const {
+  useGetDewormingPetQuery,
+  useAddNewDewormingMutation,
+  useUpdateDewormingMutation,
+  useDeleteDewormingMutation,
+  useGetDewormingQuery,
+} = dewormingApi;
