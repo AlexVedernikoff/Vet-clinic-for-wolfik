c7abb1505e9832523b4bc7c0d703974445c4e047
Amagyzener <dmitry-phs535@ya.ru>
2022-09-29 19:34:51 +1000

    project upgrade

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.scss';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+
 import App from './app/App';
-import { store } from './store';
+import { store } from './app/store';
+
+import './styles/index.scss';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
+  /* StrictMode renders components twice (on dev, but not production)
+    in order to detect any problems with the code and warn about them. */
+  <StrictMode>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </StrictMode>,
   document.getElementById('root'),
 );
---
+++ src/widgets/index.ts
@@ -0,0 +1,5 @@
+export { FormWidget } from './FormWidget/FormWidget';
+export { ForumWidget } from './ForumWidget/ForumWidget';
+export { Header } from './Header/Header';
+export { PetSidebar } from './PetSidebar/PetSidebar';
+export { PetsInfoWidget } from './PetsInfoWidget/PetsInfoWidget';
